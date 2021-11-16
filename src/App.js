a2d8a68d0c490acdd56f5f8f5f2182e8eb9054cc
Johanna Agnar <johanna.agnar@outlook.com>
2021-11-16 15:24:52 +0100

    added fetch

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
 const [username, setUsername] = useState('Johanna')
@@ -8,6 +8,8 @@ const [subject, setSubject] = useState('')
 const [messageText, setMessageText] = useState('')
 const [newMessage, setNewMessage] = useState()
 const [messageDone, setMessageDone] = useState(false)
+const [dataList, setDataList] = useState([])
+
 function handleClick(e) {
   e.preventDefault()
   setMessageDone(true)
@@ -18,6 +20,19 @@ function handleClick(e) {
   }
   setNewMessage(message)
 }
+
+useEffect(() => {
+  
+  async function getData() {
+  const response = await fetch('http://localhost:3363/api/posts') 
+  const data = response.json()
+  setDataList(data)
+
+}
+  getData()
+
+
+}, [])
 
   return (
     <div className="App">
@@ -32,7 +47,16 @@ function handleClick(e) {
          </ul> 
         </section>
         <section className="messages">
-          <article>
+
+          { dataList? dataList.map(comment => {
+            <article>
+              <h4>Username: {comment.user}</h4>
+              <h4>Time: {comment.dateTime}</h4>
+              <p>{comment.text}</p>
+            </article>
+          })
+        : 'oops kan inte nå api'}
+          {/* <article>
           <h4>Username: En kompis</h4>
           <h4>Time: 21-11-15 16:06</h4>
             <h4>Subject: Ämne</h4>
@@ -58,8 +82,8 @@ function handleClick(e) {
             <h4>Time: 21-11-15 16:06</h4>
             <h4>Subject: {newMessage.subject}</h4>
             <p>{newMessage.text}</p>
-          </article>
-          : null}
+          </article> */}
+         {/*  : null} */}
           
 
         </section>
