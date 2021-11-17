3416001705a1c065888a10dc60ed3e6488a8508a
Johanna Agnar <johanna.agnar@outlook.com>
2021-11-17 11:08:59 +0100

    post request

--- src/App.js
+++ src/App.js
@@ -6,34 +6,45 @@ function App() {
 const [username, setUsername] = useState('Johannaa')
 const [subject, setSubject] = useState('')
 const [messageText, setMessageText] = useState('')
-const [newMessage, setNewMessage] = useState()
-const [messageDone, setMessageDone] = useState(false)
+/* const [newMessage, setNewMessage] = useState()*/
+/* const [messageDone, setMessageDone] = useState(false) */
 const [dataList, setDataList] = useState([])
+const url = 'http://localhost:3363/api/posts'
 
 function handleClick(e) {
   e.preventDefault()
-  setMessageDone(true)
+/*   setMessageDone(true)
+ */  
   let message = {
-    user: username,
-    subject: subject,
-    text: messageText
+    User: username,
+    Text: messageText,
+    DateTime: new Date().toISOString().slice(0, 19).replace('T', ' ')
   }
-  setNewMessage(message)
+  addNewPost(message)
+  getData();
 }
 
 useEffect(() => {
   
   async function getData() {
-  const response = await fetch('http://localhost:3363/api/posts') 
+  const response = await fetch(url) 
   const data = await response.json()
   const one = data[0]
   setDataList(data)
 console.log(data) 
 }
   getData()
-
-
 }, [])
+
+async function addNewPost(message) {
+  await fetch(url, {
+    method: 'POST',
+    body: JSON.stringify(message),
+    headers: {
+        "Content-type": "application/json; charset=UTF-8"
+    }
+})
+}
 
   return (
     <div className="App">
@@ -49,53 +60,32 @@ console.log(data)
         </section>
         <section className="messages">
 
-    {/* <p>{dataList? dataList[0].user:null}</p> */}
           { dataList? dataList.map(comment => 
             <article key={comment.Id}>
               <h4>Username: {comment.User}</h4>
-              <h4>Time: {comment.dateTime}</h4>
+              <h4>Time: {comment.DateTime}</h4>
               <p>{comment.Text}</p>
             </article>
           )
          : 'oops kan inte nå api'}
-          {/* <article>
-          <h4>Username: En kompis</h4>
-          <h4>Time: 21-11-15 16:06</h4>
-            <h4>Subject: Ämne</h4>
-            <p>This is an old message!</p>
-          </article>
-
-          <article>
-          <h4>Username: En annan kompis</h4>
-          <h4>Time: 21-11-15 16:06</h4>
-            <h4>Subject: Ämne</h4>
-            <p>This is an old message!</p>
-          </article>
-
-          <article>
-          <h4>Username: En tredje kompis</h4>
-          <h4>Time: 21-11-15 16:06</h4>
-
-            <h4>Subject: Ämne</h4>
-            <p>This is an old message!</p>
-          </article>
+          
           {messageDone? <article>
             <h4>Username: {newMessage.user}</h4>
             <h4>Time: 21-11-15 16:06</h4>
             <h4>Subject: {newMessage.subject}</h4>
             <p>{newMessage.text}</p>
-          </article> */}
-         {/*  : null} */}
+          </article> 
+          : null} 
           
 
         </section>
         <section className="new-message">
-        {/* <form> 
+         <form> 
           <input type="text" placeholder={username} value={username}/> 
           <input type="text" placeholder="Ämne..." value={subject} onChange={e => setSubject(e.target.value)}/> 
           <input type="text" placeholder="Ditt meddelande..." onChange={e => setMessageText(e.target.value)}/> 
           <button onClick={(e)=>handleClick(e)}>Send message</button>
-        </form> */}
+        </form> 
         </section>
       </main>
     </div>
