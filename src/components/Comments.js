6d63eea4def168a0ac49d0d224f8eb241789c008
Noskig <48559023+Noskig@users.noreply.github.com>
2021-11-19 11:56:49 +0100

    Edit form style

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -9,6 +9,10 @@ function Comments(props) {
     const [username, setUsername] = useState('')
     const [messageText, setMessageText] = useState('')
     const baseUrl = 'http://localhost:3363/'
+    const [editActive, setEditActive] = useState(false)
+    const [commentActive ,setCommentActive] = useState('')
+    const [deletecommentActive ,setDeleteCommentActive] = useState('')
+    const [deleteActive, setDeleteActive] = useState(false)
 
     
     useEffect(()=>{
@@ -33,6 +37,30 @@ function Comments(props) {
         addNewComment(message)
       }    
 
+      function handleEdit(e, comment){
+          e.preventDefault()
+          setCommentActive(comment.commentid)
+          setEditActive(true)
+          setMessageText(comment.comment_text)
+      }
+
+      async function SendEdit(e, changedComment){
+        e.preventDefault()
+
+        if(changedComment.comment_text !== messageText){
+        changedComment.comment_text = messageText
+        console.log(changedComment)
+        await fetch(baseUrl+'api/comments/'+changedComment.commentid, {
+          method: 'PUT',
+          body: JSON.stringify(changedComment),
+          headers: {
+            "Content-type": "application/json",
+                }
+                })
+            }
+            
+    setEditActive(false)
+    }
       async function addNewComment(message) {
           console.log(message)
         await fetch(baseUrl+'api/comments', {
@@ -45,23 +73,65 @@ function Comments(props) {
         getComments(id);
       }
 
+      async function handleDelete(e, Cid){
+        e.preventDefault()
+        await fetch(baseUrl+'api/comments/'+Cid, {
+            method: 'DELETE',
+            body: {id:Cid},
+            headers: {
+              "Content-type": "application/json",
+            }
+        })
+        getComments(id)
+      }
+      function setActiveDelete(e, comment){
+        e.preventDefault()
+        setDeleteActive(true)
+        setDeleteCommentActive(comment.commentid)
+    }
+
+
     return (
        
         <section className="comments">
            
             {commentsList? commentsList.map(comment => <article key={comment.commentid}>
+                <div className="header">
+              <h4 className="user">{comment.user} said:</h4>
+              <h4>{comment.date}</h4>
+            </div>
+               {editActive && commentActive===comment.commentid 
+                ? <textarea value={messageText} className="form-control z-depth-1" onChange={e => setMessageText(e.target.value)}/> 
+                : <p>{comment.comment_text}</p>}
+              
+                <div className="footer text-end">
+                    {editActive && commentActive===comment.commentid 
+                    ?<button type="button" onClick={(e)=>SendEdit(e, comment)}>Done</button>
+                    :<><button type="button" onClick={(e)=>handleEdit(e, comment)}>Edit&nbsp;</button>   
+                    
+                    {deleteActive && deletecommentActive===comment.commentid
+                    ?<button type="button" onClick={(e)=>handleDelete(e, comment.commentid)}>Confirm</button>  
+                    :<button type="button" onClick={(e)=>setActiveDelete(e, comment)}>Delete</button>  
+                    }</>}
+                </div>
+         
 
-                <p>{comment.comment_text}</p>
             </article>):'no comments for this post'}
-        <div>
+        
+        <form>
+      
+        <label htmlFor="userName">User</label>
+        <input type="text" id="userName" className="form-control form-control-sm" onChange={e => setUsername(e.target.value)}/> 
 
-        <form> 
-          <input type="text" placeholder={"Namn"} className="input-name" onChange={e => setUsername(e.target.value)}/> 
-          <textarea placeholder="Ditt meddelande..."  className="input-text" onChange={e => setMessageText(e.target.value)}/> 
-          <button type="button" className="btn border border-1" onClick={(e)=>handleClick(e)}>Send message</button>
-        </form> 
-
+        <div className="form-group shadow-textarea">
+        <label htmlFor="commentArea">Message</label>  
+        <textarea className="form-control z-depth-1" id="commentArea" rows="2" onChange={e => setMessageText(e.target.value) }></textarea>
         </div>
+        <button type="button" className="btn border border-1 mt-2" onClick={(e)=>handleClick(e)}>Send message</button>
+
+        </form>
+        
+    
         </section>
        
     )
