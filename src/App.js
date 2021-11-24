31704f3896cc47d4ce4bfadae1e247c47bc81261
Daniel <danielkhosh9@gmail.com>
2021-11-24 06:50:23 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -1,35 +1,13 @@
-<<<<<<< HEAD
-import './App.css';
-import React from 'react';
-=======
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Discussion from './components/Discussion';
 import CreateTopicpage from './components/Homepage/CreateTopicpage';
->>>>>>> 301476d6a2efd2b829a6dc221d8b293e643a0d17
 import {
   BrowserRouter as Router,
   Routes,
   Route,
 } from "react-router-dom";
 
-<<<<<<< HEAD
-import Conversationpage from './pages/Conversation';
-import CreateNewTopicpage from "./pages/CreateTopicpage";
-
-
-function App() {
-  return (
-    <Router>
-      <div className="App">
-        <Routes>
-          <Route exact path="/" element={<Conversationpage/>} />
-          <Route path="/topic" element={<CreateNewTopicpage/>} />
-        </Routes>
-      </div>
-    </Router>
-  );
-=======
 function App() {
   return(
     <Router>
@@ -41,6 +19,5 @@ function App() {
       </div>
     </Router>
   )
->>>>>>> 301476d6a2efd2b829a6dc221d8b293e643a0d17
 }
 export default App;
